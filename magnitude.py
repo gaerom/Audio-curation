ddd70587e0f0496543d7cbfd9544c2129d372b9e
gaerom <rlatofha3597@gmail.com>
2024-04-03 15:12:21 +0900

    mean, median, max 결과 저장

--- magnitude.py
+++ magnitude.py
@@ -1,9 +1,4 @@
-"""
-추가해야 할 내용
-1. 현재는 각각의 audio에 대한 magnitude의 mean값만 계산해서 저장
-    a. 각각의 audio에 대한 중간값을 추가로 저장해야 함.
-2. 그리고 각각의 audio magnitude의 평균과 중간값을 구했다면, 전체 값에 대한 평균과 중간값도 구하기
-"""
+
 
 import os
 import numpy as np 
@@ -13,13 +8,17 @@ import matplotlib.pyplot as plt
 
 # audio_dir = '/mnt/storage1/test'
 audio_dir = '/mnt/storage1/vgg_dataset/raw_audios'
-output_file = 'max_amplitude_values.txt'
+# output_file = 'max_amplitude_values.txt'
+output_file = 'mean_median_max.txt'
 total_result = 'results.txt'
 
 # audio file
 audio_files = [f for f in os.listdir(audio_dir) if f.endswith('.wav')]
 
+mean_magnitudes = {}
+median_magnitudes = {}
 max_magnitudes = {}
+
 
 # 전체 파일에 대한 값들
 all_mean = []
@@ -37,16 +36,18 @@ for file in audio_files:
     # 하나의 audio file -> mean, median 계산
     mean_magnitude = np.mean(magnitude_spectrum)
     median_magnitude = np.median(magnitude_spectrum)
+    max_magnitude = np.max(magnitude_spectrum)
+
+    mean_magnitudes[file] = mean_magnitude
+    median_magnitudes[file] = median_magnitude
+    max_magnitudes[file] = max_magnitude
     
     all_mean.append(mean_magnitude)
     all_median.append(median_magnitude)
-    
-    # print(f'{file}: Mean: {mean_magnitude}, Median: {median_magnitude}')
-    
-    max_magnitude = np.max(magnitude_spectrum)
-    max_magnitudes[file] = max_magnitude
     all_max.append(max_magnitude) # 최댓값들 모아놓은 list
     
+    # print(f'{file}: Mean: {mean_magnitude}, Median: {median_magnitude}')
+   
     print(f'{file}: {max_magnitude}') # 계산된 magnitude에서 최댓값 확인
     
 # 전체에 대한 mean, median 계산
@@ -58,8 +59,8 @@ total_max_mean = np.mean(all_max)
 
         
 with open(output_file, 'w') as f:
-    for file, magnitude in max_magnitudes.items():
-        f.write(f'{file}: {magnitude}\n')
+    for file in audio_files:
+        f.write(f'{file}: Mean: {mean_magnitudes[file]} / Median: {median_magnitudes[file]} / Max: {max_magnitudes[file]}\n')
 
 with open(total_result, 'w') as f:
     f.write(f'전체 mean: {total_mean}, 전체 median: {total_median}, 최댓값의 mean: {total_max_mean}\n')
